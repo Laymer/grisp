876b303969b8b269c7452a40c25aac31db9db2dc
Julien Bastin <julien.bastin@student.uclouvain.be>
2020-06-04 00:55:04 +0200

    triggering sonar value works

--- src/pmod_maxsonar.erl
+++ src/pmod_maxsonar.erl
@@ -31,7 +31,7 @@
 
 %--- Records -------------------------------------------------------------------
 
--record(state, {port, last_val, txd_pin_state}).
+-record(state, {port, last_val}).
 
 %--- API -----------------------------------------------------------------------
 
@@ -43,7 +43,7 @@ start_link(Slot, _Opts) ->
 -spec get() -> integer().
 get() ->
     gen_server:call(?MODULE, get_value).
-% @doc Get the latest measured distance in inches.
+
 -spec get_single() -> integer().
 get_single() ->
     gen_server:call(?MODULE, get_single_value).
@@ -55,65 +55,51 @@ init(Slot = uart) ->
     Port = open_port({spawn_driver, "grisp_termios_drv"}, [binary]),
     grisp_devices:register(Slot, ?MODULE),
     grisp_gpio:configure(uart_2_txd, output_0),
-    {ok, #state{
-        port = Port
-        , txd_pin_state = output_0
-        , last_val = undefined}}.
+    {ok, #state{port = Port}}.
 
 % @private
 handle_call(get_value, _From, #state{last_val = Val} = State) ->
     {reply, Val, State};
-% @private
-handle_call(get_single_value
-    , _From
-    , #state{last_val = LastVal, txd_pin_state = Txd} = State) ->
-
-    Single = case LastVal =:= undefined orelse Txd =:= output_0 of
-        true -> %% first measurement
-            grisp_gpio:configure(uart_2_txd, output_1),
-            timer:sleep(50), %% just in case, maybe not necessary
-            pmod_maxsonar:get();
-        _ ->
-            LastVal
-    end,
+handle_call(get_single_value, _From, #state{port = Port} = State) ->
+    grisp_gpio:configure(uart_2_txd, output_1),
     grisp_gpio:configure(uart_2_txd, output_0),
-    {reply
-        , Single
-        , State#state{last_val = Single, txd_pin_state = output_0}
-        , 5000}.
+    Val = receive
+              {Port, {data, Data}} ->
+                  decode(Data, State)
+          end,
+    {reply, Val, State#state{last_val = Val}}.
 
 % @private
 handle_cast(Request, _State) -> error({unknown_cast, Request}).
 
 % @private
 handle_info({Port, {data, Data}}, #state{port = Port} = State) ->
-    case Data of
-        % Format of response is 'Rxxx\n' where xxx is the decimal
-        % representation of the measured range in inches (2.54cm)
-        % (left-padded with zeros - so there are always three digits)
-        <<$R, D1, D2, D3, $\n>> when $0 =< D1, D1 =< $9,
-                                      $0 =< D2, D2 =< $9,
-                                      $0 =< D3, D3 =< $9 ->
-            % Val is given in inches
-            Val = (D1 - $0) * 100 + (D2 - $0) * 10 + (D3 - $0),
-            {noreply, State#state{last_val = Val}};
-        % Sometimes for no obvious reason we receive
-        % a different value from the sonar.
-        % Instead of $R we get two garbage characters
-        <<_, _, D1, D2, D3, $\n>> when $0 =< D1, D1 =< $9,
-                                      $0 =< D2, D2 =< $9,
-                                      $0 =< D3, D3 =< $9 ->
-            % Val is given in inches
-            Val = (D1 - $0) * 100 + (D2 - $0) * 10 + (D3 - $0),
-            {noreply, State#state{last_val = Val}};
-        _ ->
-            {noreply, State}
-    end.
-
-
+    {noreply, State#state{last_val = decode(Data, State)}}.
 
 % @private
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
 % @private
 terminate(_Reason, _State) -> ok.
+
+decode(Data, State) ->
+    case Data of
+        % Format of response is 'Rxxx\n' where xxx is the decimal
+        % representation of the measured range in inches (2.54cm)
+        % (left-padded with zeros - so there are always three digits)
+        <<$R, D1, D2, D3, $\n>> when $0 =< D1, D1 =< $9,
+            $0 =< D2, D2 =< $9,
+            $0 =< D3, D3 =< $9 ->
+            % Val is given in inches
+            (D1 - $0) * 100 + (D2 - $0) * 10 + (D3 - $0);
+        % Sometimes for no obvious reason we receive
+        % a different value from the sonar.
+        % Instead of $R we get two garbage characters
+        <<_, _, D1, D2, D3, $\n>> when $0 =< D1, D1 =< $9,
+            $0 =< D2, D2 =< $9,
+            $0 =< D3, D3 =< $9 ->
+            % Val is given in inches
+            (D1 - $0) * 100 + (D2 - $0) * 10 + (D3 - $0);
+        _ ->
+            State#state.last_val
+    end.
